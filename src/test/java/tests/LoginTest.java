11bfa4759407eb92be2f07c200322ecd86ee1811
kareem261 <64379540+kareem261@users.noreply.github.com>
2022-09-15 19:47:08 +0200

    Add test data files

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -7,6 +7,11 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.HomePage;
 import pages.LoginPage;
+import pages.UserRegistrationPage;
+import utils.JsonFileManager;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static utils.BrowserAction.closeAllBrowserTabs;
 import static utils.BrowserFactory.getDriver;
@@ -14,17 +19,27 @@ import static utils.BrowserFactory.getDriver;
 
 public class LoginTest{
 
+    JsonFileManager JsonObject;
     HomePage HomeObject;
+    UserRegistrationPage UserRegisterObject;
     LoginPage LoginObject;
     WebDriver driver;
+
+    String currentTime = new SimpleDateFormat("ddMMyyyyHHmmssSSS").format(new Date());
 
     //need to be maintained it should start it from register
     @Test
     public void CheckLoginSuccessfully(){
         HomeObject.openLoginPage();
-        LoginObject.Login("Mohamed@gmail.com","tester123");
+        String email = JsonObject.getTestData("users.RegisteredEmail")+"@"+JsonObject.getTestData("users.emailDomain");
+        String password = JsonObject.getTestData("users.Password");
+
+      //  UserRegisterObject.registerWithRequiredFields(JsonObject.getTestData("firstname"),
+        //        JsonObject.getTestData("LastName"),email,password,password);
+
+        LoginObject.Login(email,password);
         String Result = HomeObject.CheckLougoutLink();
-        Assert.assertEquals(Result,"Log out");
+        Assert.assertEquals(Result, JsonObject.getTestData("messages.LoginSuccessfully"));
     }
 
     @Test
@@ -34,14 +49,17 @@ public class LoginTest{
         String Result = LoginObject.getLoginErrorMessage();
         Assert.assertEquals(Result,"Login was unsuccessful. Please correct the errors and try again.\n" +
                 "No customer account found");
+
     }
 
     @BeforeMethod
     public void setUp(){
+        JsonObject = new JsonFileManager("src/test/data/LoginTestData.json");
         driver = getDriver("chrome");
 
         //here we initialize all pageObject classes that we will be using
         HomeObject = new HomePage(driver);
+        UserRegisterObject = new UserRegistrationPage(driver);
         LoginObject = new LoginPage(driver);
 
         HomeObject.navigateToHomePage();
